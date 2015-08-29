6857d04ab68a27422a73b67f20bfec29b7ba077b
erikasf <erika@erikasf.com>
2015-08-28 20:38:57 -0700

    slowly working my way through learning jade

--- src/app.js
+++ src/app.js
@@ -1,42 +1,41 @@
 'use strict';
 
 var express = require('express'),
-	  posts = require('./mock/posts.json');
+	  posts = require('./posts.json');
 
-var postsLists = Object.keys(posts).map(function(value) {
-							         return posts[value]})
+			         
 
 var app = express();
 
-app.use('/static', express.static(__dirname + '/public'))
 
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res){
-	var path = req.path;
-	res.locals.path = path;
-	res.render('index');
+	res.send("hello world")
 });
+app.get('/blog', function(req,res){
+// 	res.send("this is the main blog page")
 
-app.get('/blog/:title?', function(req, res){ 
-	var title = req.params.title;
-	if (title === undefined) {
-		res.status(503);
-		res.render('blog', {posts: postsLists})
-	} else {
-		var post = posts[title] || {};
-		res.render('post', { post: post});
-	}
-});
-
-app.get('/posts', function(req, res) {
-	if (req.query.raw) {
-		res.json(posts);
-	} else {
-		res.json(postsLists);
-	}
+ });
+app.get('/blog/ :new', function(req,res){
+	res.send('this is the new posts page');
 })
+
+app.get('/blog/:title',function(req, res){
+	debugger
+	var title = req.params.title;
+	if(title === undefined){
+		res.status(503);
+		res.send("this is under construction")
+	} else{
+	
+	var post = posts[title];
+	res.send(posts);
+};
+
+});
+
+
+
 
 app.listen(3000, function() {
 	console.log("The frontend server is running on port 3000!");
