2a45396050331e94ecfe72b3f6a881524dcda238
erikasf <erika@erikasf.com>
2015-08-29 18:44:15 -0700

    updating blog

--- src/app.js
+++ src/app.js
@@ -2,41 +2,50 @@
 
 var express = require('express'),
 	  posts = require('./posts.json');
+	 
 
 			         
 
 var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
 
 
 
 app.get('/', function(req, res){
-	res.send("hello world")
+	res.render('index')
 });
 app.get('/blog', function(req,res){
 // 	res.send("this is the main blog page")
+	res.render("blog")
 
  });
-app.get('/blog/ :new', function(req,res){
-	res.send('this is the new posts page');
-})
+// app.get('/blog/ :new', function(req,res){
+// 	res.send('this is the new posts page');
+// })
 
 app.get('/blog/:title',function(req, res){
-	debugger
+
 	var title = req.params.title;
+
 	if(title === undefined){
 		res.status(503);
 		res.send("this is under construction")
 	} else{
 	
-	var post = posts[title];
-	res.send(posts);
+	var post = posts[title] || {};
+	res.render('post',{post:post})
+	
 };
 
-});
+
+
 
 
 
 
 app.listen(3000, function() {
 	console.log("The frontend server is running on port 3000!");
+
+});
 });
